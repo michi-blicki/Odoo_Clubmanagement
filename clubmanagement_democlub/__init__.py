9146da0723cc5d73d6e223b478f0c5f6df41562b
Michi Blickenstorfer <michi@blicki.ch>
2026-02-07 07:45:22 +0100

    1.0.3: automatic user creation

--- clubmanagement_democlub/__init__.py
+++ clubmanagement_democlub/__init__.py
@@ -10,7 +10,6 @@ _logger = logging.getLogger(__name__)
 
 
 from .models.club_member_postinit import generate_club_members
-from .models.club_member_postinit import create_demo_user_members
 
 def _pre_init_hook(env):
     _logger.info(f"_pre_init_hook(): Start")
@@ -423,8 +422,6 @@ def _post_init_hook(env):
         
     ]
     generate_club_members(env, team_config)
-
-    create_demo_user_members(env)
 
     _logger.info(f"_post_init_hook(): End")
 
